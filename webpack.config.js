73bb14315d627d8143f76d0449d0689215ceb96c
Adam Miller <chadam189@gmail.com>
2018-01-22 19:54:01 -0800

    Configure webpack dev server (similar to nodemon, but for WB builds)

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "js/[name].js"
   },
+  devServer: {
+    contentBase: "./dist"
+  },
   module: {
     rules: [
       {
